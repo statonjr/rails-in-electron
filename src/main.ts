d44bae753943216abc859c1d02c34a59e04e3e74
Larry Staton Jr <larry@larrystaton.com>
2020-12-27 23:46:02 -0500

    Introduce TypeScript
    Use TS for `main` electron file. We will use TS with Rails where we can.

--- src/main.ts
+++ src/main.ts
@@ -1,50 +1,52 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
-const path = require('path')
-const fetch = require('node-fetch')
+import { app, BrowserWindow } from 'electron';
+import fetch from 'node-fetch';
+import path from 'path';
+import * as childProcess from 'child_process';
 
-function createWindow (railsApp) {
+const createWindow = (railsApp: any) => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1200,
+    height: 800,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
+      contextIsolation: true
+      // preload: path.join(__dirname, 'preload.js')
     }
-  })
+  });
 
   // and load the index.html of the app.
-  mainWindow.loadURL('http://localhost:3000')
+  mainWindow.loadURL('http://localhost:3000');
 
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools()
 
    // Emitted when the window is closed.
    mainWindow.on('closed', function() {
     railsApp.kill('SIGINT')
-  })
+  });
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  const railsApp = require('child_process').spawn('rails', ['s'])
+  const railsApp = childProcess.spawn('rails', ['s']);
 
-  console.log("starting rails server... waiting 5 seconds")
+  console.log("Starting Rails server. Waiting 5 seconds...")
 
-  async function start() {
-    const res = await fetch('http://localhost:3000')
-    console.log('rails server started')
-    createWindow(railsApp)
-    app.on('activate', function () {
+  const start = async () => {
+    const res = await fetch('http://localhost:3000');
+    console.log('Rails server started');
+    createWindow(railsApp);
+    app.on('activate', () => {
       // On macOS it's common to re-create a window in the app when the
       // dock icon is clicked and there are no other windows open.
       if (BrowserWindow.getAllWindows().length === 0) createWindow(railsApp)
-    })
+    });
   }
 
-  setTimeout(() => start(), 5000)
+  setTimeout(() => start(), 5000);
 })
 
 // Quit when all windows are closed, except on macOS. There, it's common
@@ -52,7 +54,4 @@ app.whenReady().then(() => {
 // explicitly with Cmd + Q.
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') app.quit()
-})
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+});
